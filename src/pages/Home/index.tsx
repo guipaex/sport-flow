3c462bee807ed9b4f3880dce4390bdf78d6a99d2
Guilherme Paes <paes.guilherme@hotmail.com>
2023-10-07 03:01:44 -0300

    style: update home page style

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,20 @@
 import RacesView from "../../components/RacesView";
+import style from "./styles.module.css";
+import bannerImage from "../../assets/img/foto-banner.jpg";
 
 export default function Home() {
   return (
-    <>
-      <h1>Home Page</h1>
+    <section className={style.container}>
+      <header className={style.header}>
+        <h2>Logo</h2>
+        <nav>
+          <button className={style.loginBtn}>Log In com Google</button>
+        </nav>
+      </header>
+      <div className={style.imgContainer}>
+        <img className={style.img} src={bannerImage} alt='night run' />
+      </div>
       <RacesView />
-    </>
+    </section>
   );
 }
