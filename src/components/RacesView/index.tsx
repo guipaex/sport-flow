41820f2d3bd0e377b372a37b8a5679aef9bb3021
Guilherme Paes <paes.guilherme@hotmail.com>
2023-10-11 08:18:20 -0300

    style: update races gallery and cleaning the code

--- src/components/RacesView/index.tsx
+++ src/components/RacesView/index.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import style from "./styles.module.css";
+import style from "./styles.module.scss";
 import { db } from "../../services/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import RaceThumb from "../RaceThumb";
 import corridas from "../../utils/corridasRJ.json";
+import RaceCard from "../RaceCard";
 
 const RacesView = () => {
   const [races, setRaces] = useState<Object>();
@@ -19,44 +20,14 @@ const RacesView = () => {
     console.log(corrida["event-title"]);
   });
   return (
-    <>
-      <h1>Corridas</h1>
-      <div className={style.races}>
+    <section className={style.container}>
+      <h1 className={style.title}>Corridas</h1>
+      <div className={style.gallery}>
         {Object.values(corridas).map((evento) => {
-          return (
-            <div key={evento["event-title"]} className={style.raceCard}>
-              <RaceThumb raceTHUMB={evento["event-thumb"]} />
-              <h4 className={style.raceCard__title}>{evento["event-title"]}</h4>
-              <div className={style.raceCard__infos}></div>
-              <a className={style.linkBtn} href={evento["event-page"]} target='_blank'>
-                Saiba Mais
-              </a>
-            </div>
-          );
+          return <RaceCard />;
         })}
-        {/* {races === undefined
-          ? ""
-          : Object.values(races).map((race) => {
-              return (
-                <div key={race.id} className={style.raceCard}>
-                  <RaceThumb raceID={race.id} />
-                  <h4 className={style.raceCard__title}>{race.name}</h4>
-                  <div className={style.raceCard__infos}>
-                    {Object.values(race.distances).map((distance, index) => (
-                      <p className={style.raceCard__km} key={index}>
-                        {String(distance)}Km
-                      </p>
-                    ))}
-                  </div>
-                  <a className={style.linkBtn} href={race.link} target='_blank'>
-                    Saiba Mais
-                  </a>
-                </div>
-              );
-            })} */}
-        {}
       </div>
-    </>
+    </section>
   );
 };
 
