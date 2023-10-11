058865da24c9de7c8c975ebfe9755787dc4fcb7e
Guilherme Paes <paes.guilherme@hotmail.com>
2023-10-11 07:59:19 -0300

    style: update banner and imports variables

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,15 @@
 import RacesView from "../../components/RacesView";
-import style from "./styles.module.css";
-import bannerImage from "../../assets/img/foto-banner.jpg";
+import style from "./styles.module.scss";
 
 export default function Home() {
   return (
     <section className={style.container}>
       <div className={style.imgContainer}>
-        <img className={style.banner} src={bannerImage} alt='Pessoas correndo na Night Run 2022' />
+        <img
+          className={style.banner}
+          src='https://www.webrun.com.br/wp-content/uploads/2021/05/AdobeStock_292063777.jpeg'
+          alt='Pessoa correndo sozinha'
+        />
       </div>
       <RacesView />
     </section>
