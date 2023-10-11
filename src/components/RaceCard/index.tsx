b505540adb2aa7ceb4603be67da3aa72f82025c1
Guilherme Paes <paes.guilherme@hotmail.com>
2023-10-11 11:21:24 -0300

    refactor: create a component for Races Card

--- src/components/RaceCard/index.tsx
+++ src/components/RaceCard/index.tsx
@@ -3,31 +3,72 @@ import { IRaceCardData } from "../../utils/interfaces";
 import { FaRegCalendar } from "react-icons/fa";
 import { PiSneakerMoveBold } from "react-icons/pi";
 
-export default function RaceCard(data: IRaceCardData) {
-  const eventPage =
-    "https://www.ticketsports.com.br/e/circuito-rei-e-rainha-do-+A2:G3mar-2023-35381?termo=&periodo=0&mes=0&inicio=&fim=&ordenacao=";
-  const eventThumb =
-    "https://cdn.ticketagora.com.br/ticketagora/images/thumb/EED9DQ5HLEBS0VO3C9HLQ1JXDD7DPEEWMNGR47N3LMMC3L42SK.png";
+// enum Months {
+//   JAN = "01",
+//   FEV  = "02" ,
+//   MAR  = "03",
+//   ABR  = "04",
+//   MAI  = "05",
+//   JUN  = "06",
+//   JUL  = "07",
+//   AGO  = "08",
+//   SET  = "09",
+//   OUT  = "10",
+//   NOV  = "11",
+//   DEZ  = "12",
+// }
+
+enum Months {
+  JAN = 1,
+  FEV,
+  MAR,
+  ABR,
+  MAI,
+  JUN,
+  JUL,
+  AGO,
+  SET,
+  OUT,
+  NOV,
+  DEZ,
+}
+export default function RaceCard(raceData: any) {
+  const data = raceData.raceData;
+  let distances = [];
+  data.distances.forEach((element) => {
+    distances.push(element.replace("km", ""));
+  });
+
+  const formatedData = () => {
+    const dataValues = data.date.split("-");
+    const formatedMoth = parseInt(dataValues[1]);
+
+    return `${dataValues[2]} ${Months[formatedMoth]} ${dataValues[0]}`;
+  };
+
   return (
-    <a className={style.raceCard} href={eventPage}>
+    <a className={style.raceCard} href={data.link}>
       <p className={style.sport}>
         <PiSneakerMoveBold className={style.sport__icon} />
       </p>
-      <img src={eventThumb} className={style.eventThumb} alt='Capa do Evento' />
+      <img src={data.imgURL} className={style.eventThumb} alt='Capa do Evento' />
       <h4 className={style.date}>
-        <FaRegCalendar /> 29 NOV 2023
+        <FaRegCalendar /> {formatedData()}
       </h4>
-      <h2 className={style.title}>CIRCUITO REI E RAINHA DO MAR 2023</h2>
+      <h2 className={style.title}>{data.name}</h2>
       <ul className={style.distance}>
-        <li className={style.distance__tag}>5 km</li>
-        <li className={style.distance__tag}>10 km</li>
-        <li className={style.distance__tag}>21 km</li>
-        <li className={style.distance__tag}>42 km</li>
+        {distances.map((distance) => (
+          <li key={distance} className={style.distance__tag}>
+            {distance} km
+          </li>
+        ))}
       </ul>
       <span className={style.moreInfo}>
-        <p className={style.local}>Sorocaba SP</p>
+        <p className={style.local}>
+          {data.city} - {data.state}
+        </p>
         <p className={style.priceTag}>
-          A partir de <span className={style.priceTag__value}>R$ 175,60</span>
+          A partir de <span className={style.priceTag__value}>{data.value}</span>
         </p>
       </span>
     </a>
--- src/components/RacesView/index.tsx
+++ src/components/RacesView/index.tsx
@@ -2,30 +2,30 @@ import React, { useEffect, useState } from "react";
 import style from "./styles.module.scss";
 import { db } from "../../services/firebase";
 import { collection, getDocs } from "firebase/firestore";
-import RaceThumb from "../RaceThumb";
-import corridas from "../../utils/corridasRJ.json";
 import RaceCard from "../RaceCard";
+import { IRaceCardData } from "../../utils/interfaces";
 
 const RacesView = () => {
   const [races, setRaces] = useState<Object>();
+
   const racesCollection = collection(db, "races");
   const getRaces = async () => {
     const data = await getDocs(racesCollection);
     setRaces(data.docs.map((item) => ({ ...item.data(), id: item.id })));
   };
+
   useEffect(() => {
     getRaces();
   }, []);
-  corridas.forEach((corrida) => {
-    console.log(corrida["event-title"]);
-  });
   return (
     <section className={style.container}>
       <h1 className={style.title}>Corridas</h1>
       <div className={style.gallery}>
-        {Object.values(corridas).map((evento) => {
-          return <RaceCard />;
-        })}
+        {races === undefined
+          ? ""
+          : Object.values(races).map((race) => {
+              return <RaceCard key={race.id} raceData={race} />;
+            })}
       </div>
     </section>
   );
