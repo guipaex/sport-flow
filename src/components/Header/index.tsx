734c6bfa2a2672e064c3198516bd89e0ebe2b1a2
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-10 01:39:23 -0300

    style: set header responsive and add brand colors

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,59 @@
 import style from "./header.module.scss";
+import React from "react";
+import logo from "../../assets/svg/logo_light.svg";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+// import { Link } from "react-scroll";
+import { PiListBold, PiXBold } from "react-icons/pi";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden"; // Bloqueia o scroll
+    } else {
+      document.body.style.overflow = "auto"; // Restaura o scroll
+    }
+    return () => {
+      document.body.style.overflow = "auto"; // Certifique-se de restaurar o scroll ao desmontar o componente
+    };
+  }, [isOpen]);
   return (
     <header className={style.header}>
-      <h2 className={style.logo}>BR Sports</h2>
+      <Link to='/' onClick={() => setIsOpen(false)}>
+        <img src={logo} className={style.logo} alt='Logo Sports Flow' />
+      </Link>
+      <input type='text' placeholder='Procurar...' className={style.search} />
+      <nav className={style.menu}>
+        <button className={style.menuButton} onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <PiXBold className={style.menuButton__close} /> : <PiListBold className={style.menuButton__open} />}
+        </button>
+        <ul
+          className={classNames({
+            [style.menu__links]: true,
+            [style["menu__links--open"]]: isOpen,
+            [style["menu__links--closed"]]: !isOpen,
+          })}
+        >
+          <li>
+            <Link className={style.menu__item} to='about' onClick={() => setIsOpen(!isOpen)}>
+              Sobre
+            </Link>
+          </li>
+          <li>
+            <Link className={style.menu__item} to='projects' onClick={() => setIsOpen(!isOpen)}>
+              Corridas
+            </Link>
+          </li>
+          <li>
+            <Link className={style.menu__item} to='contact' onClick={() => setIsOpen(!isOpen)}>
+              Contato
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
