eeb596937a63482d0ce52190273981d9455e4cb2
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-14 00:02:24 -0300

    chore: add content to about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,17 +1,27 @@
-import RacesView from "../../components/RacesView";
-import style from "./styles.module.scss";
+import { useState } from "react";
+import { GrInstagram } from "react-icons/gr";
+import { ContactValues } from "../../utils/interfaces";
+import style from "./about.module.scss";
 
 export default function About() {
   return (
     <section className={style.container}>
-      <div className={style.imgContainer}>
-        <img
-          className={style.banner}
-          src='https://www.webrun.com.br/wp-content/uploads/2021/05/AdobeStock_292063777.jpeg'
-          alt='Pessoa Correndo ao pôr do Sol'
-        />
+      <h1 className={style.title}>A maior plataforma de esportes do Brasil!</h1>
+      <div className={style.content}>
+        <h2>Esse é o nosso objetivo!</h2>
+        <p>
+          E para chegar lá, estamos construindo uma plataforma completa para atletas de todas as modalidades, seja{" "}
+          <strong>Corrida de Rua</strong>, <strong>Natação</strong>, <strong>Ciclismo</strong>, <strong>Triatlo</strong>{" "}
+          e diversas outras modalidades.
+        </p>
+        <p>
+          Siga-nos no Instagram e fique por dentro das atualizações
+          <br />
+          <a className={style.ig} href='https://www.instagram.com/sportflow.br/'>
+            <GrInstagram className={style.ig__icon} /> @sportflow.br{" "}
+          </a>
+        </p>
       </div>
-      <RacesView />
     </section>
   );
 }
