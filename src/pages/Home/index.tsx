e02d7eaa043682e29a394f0f99a3d9e919806d91
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-21 22:03:13 -0300

    chore: remove unused code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,6 @@
 import RacesView from "../../components/RacesView";
 import style from "./styles.module.scss";
 
-const sports = ["Corrida de Rua", "Natação", "Ciclismo", "Triatlo"];
-
 export default function Home() {
   return (
     <section className={style.container}>
