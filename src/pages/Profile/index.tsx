75b95e4baedb0b1b36feb1920793d87638f77d46
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-29 20:29:56 -0300

    refactor: separate diferent components for logged user and visitor

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,31 +1,50 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import style from "./profile.module.scss";
 import React, { useEffect, useState, useContext } from "react";
 import { AuthGoogleContext } from "../../contexts/authGoogle";
 import { db } from "../../services/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import { query, collection, where, getDocs } from "firebase/firestore";
+import userData from "../../utils/interfaces/userData";
 import ProfileView from "./ProfileView";
+import UserProfile from "./ProfilePanel";
 
 export default function Profile() {
   const { user } = useContext<any>(AuthGoogleContext);
   const { username } = useParams();
-  const [userData, setUserData] = useState<any>();
-  const [status, setStatus] = useState(false);
+  const [userData, setUserData] = useState<userData>();
+  const [status, setStatus] = useState<string>("Loading...");
 
-  async function getUserData() {
-    const ref = doc(db, "users", user);
-    const data = await getDoc(ref);
-    if (data.exists()) {
-      console.log(data.data());
+  async function getUserData(userRef) {
+    const ref = query(collection(db, "users"), where("username", "==", userRef));
+    const data = await getDocs(ref);
+    if (data.docs.length === 1) {
+      const result: userData = data.docs[0].data();
+      setUserData(result);
+      setStatus("OK");
     } else {
+      setStatus("404");
       setUserData(undefined);
-      setStatus(false);
     }
   }
 
   useEffect(() => {
-    getUserData();
-  }, []);
+    getUserData(username);
+  }, [user]);
 
-  return <ProfileView user={username} />;
+  if (status === "404") {
+    return (
+      <main className={style.container}>
+        <h1>Perfil de não encontrado</h1>
+      </main>
+    );
+  } else if (status === "Loading...") {
+    return (
+      <main className={style.container}>
+        <h1>Carregando...</h1>
+      </main>
+    );
+  } else if (status === "OK") {
+    return username === user ? <UserProfile data={userData} /> : <ProfileView data={userData} />;
+  }
 }
