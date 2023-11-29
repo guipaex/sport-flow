890294eea081a10acc85ed1a92997af531cea870
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-29 00:02:13 -0300

    feat: create user in db

--- src/contexts/authGoogle.js
+++ src/contexts/authGoogle.js
@@ -2,7 +2,7 @@ import {useState, useEffect, createContext} from 'react';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app } from "../services/firebase";
 import { db } from "../services/firebase";
-import { doc, getDoc  } from "firebase/firestore";
+import { doc, getDoc, setDoc, addDoc, collection } from "firebase/firestore";
 
 const provider = new GoogleAuthProvider();
 export const AuthGoogleContext = createContext({});
@@ -18,34 +18,50 @@ export const AuthGoogleProvider = ({ children }) => {
     
     async function loadAuthData(){
       const sessionToken = sessionStorage.getItem("@AuthFirebase:token");
-      const sessionUser = sessionStorage.getItem("@AuthFirebase:user");
+      const sessionUser = sessionStorage.getItem("@userID");
       const user = sessionUser;
       if(sessionToken && sessionUser){
         const userid = await getUserName(user)
         setSigned(true)
         setUser(userid)
-        
       }
     
+  }
+async function createUser(user){
+  console.log(user)
+  function splitName(name) {
+    const splitted = name.split(" ");
+    return splitted;
+  }
+
+  const userData = {
+    name: splitName(user.displayName)[0],
+    lastName:splitName(user.displayName)[1],
+    email: user.email,
+  }
+  console.log(userData)
+  const newUser = await setDoc(doc(db, "users", user.uid), userData);
   }
   async function getUserName(uid){
       const userData = await getDoc(doc(db, "users", uid));
       const data = userData.data();
       if (data && data.username) {
         return data.username;
-      } else {
+      } else{
+        const userData = sessionStorage.getItem('@userData')
+        createUser(JSON.parse(userData))
         return uid
       }
   }
 	const signInGoogle = () => {
-    console.log('chamou')
     signInWithPopup(auth, provider)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
         const user = result.user;
         sessionStorage.setItem("@AuthFirebase:token", token);
-        sessionStorage.setItem("@AuthFirebase:user", user.uid)
+        sessionStorage.setItem("@userID", user.uid)
+        sessionStorage.setItem("@userData", JSON.stringify(user))
         window.location.reload();
       })
       .catch((error) => {
