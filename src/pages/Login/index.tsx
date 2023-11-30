840cf32d94dcddba9d5cecbacb9399a39a15f5b7
Guilherme Paes <paes.guilherme@hotmail.com>
2023-11-29 21:57:02 -0300

    :sparkles: feat: add Login Page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,10 @@
-import { LoginGoogle } from "../../components/buttons";
 import style from "./login.module.scss";
-import { FcGoogle } from "react-icons/fc";
-import { useContext } from "react";
-import { AuthGoogleContext } from "../../contexts/authGoogle";
+import LoginForm from "../../components/Form/Login/Login";
 
 export default function Login() {
-  return <main className={style.container}></main>;
+  return (
+    <main className={style.container}>
+      <LoginForm />
+    </main>
+  );
 }
